3187dd6798c064ef6c27a8f7d80622cf05a268f2
Charlotte Marion <marion.charlotte@orange.fr>
2022-04-08 16:35:13 +0200

    creation routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
+// module
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
+// composants
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CocktailContainerComponent } from './cocktail-container/cocktail-container.component';
@@ -9,6 +12,9 @@ import { CocktailDetailsComponent } from './cocktail-container/cocktail-details/
 import { SelectedDirective } from './shared/directives/selected.directive';
 import { PanierContainerComponent } from './panier-container/panier-container.component';
 import { IngredientListComponent } from './panier-container/ingredient-list/ingredient-list.component';
+
+// routes
+import { APP_ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -22,7 +28,8 @@ import { IngredientListComponent } from './panier-container/ingredient-list/ingr
     IngredientListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(APP_ROUTES),
   ],
   providers: [],
   bootstrap: [AppComponent]
